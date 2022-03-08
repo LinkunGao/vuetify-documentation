96b74a56df68b3ccafe06542e40759914f78acbd
LinkunGao <gaolinkun123@gmail.com>
2022-03-09 12:41:36 +1300

    start to lesson04

---
+++ todo-lesson04/src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-app-bar-nav-icon
+        class="grey--text"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn depressed class="grey lighten-4 grey--text">
+        <span>Sign Out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" app class="primary" temporary>
+      <v-list>
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+        >
+          <v-list-item-action>
+            <v-icon class="white--text"> {{ link.icon }} </v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text font-weight-bold">
+              {{ link.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: "mdi-view-dashboard", text: "Dashboard", route: "/" },
+        { icon: "mdi-folder", text: "My Projects", route: "/projects" },
+        { icon: "mdi-account-group", text: "Team", route: "/team" },
+      ],
+    };
+  },
+};
+</script>
